aea378e9d5f1f1074b9413f54a8e869e29fe5ae9
Dhiza Wahyu Firmansyah <50905117+dhizafirmansyah@users.noreply.github.com>
2025-12-19 06:22:40 +0700

    Set base path to /demo_gate/ for assets
    Updated vite.config.ts to set the base path to '/demo_gate/' to fix 404 errors for static assets. Adjusted asset paths in dist/index.html to match the new base path.

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Baris di bawah ini adalah kunci untuk memperbaiki Error 404
+  base: '/demo_gate/', 
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
